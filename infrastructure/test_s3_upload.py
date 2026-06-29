14f44ce13027903c96db88fd95930d78d16caad0
Tyler Beede <tbeede@usc.edu>
2026-06-28 20:52:26 -0700

    Updated S3 upload script

--- infrastructure/test_s3_upload.py
+++ infrastructure/test_s3_upload.py
@@ -23,42 +23,6 @@ s3 = boto3.client(
     aws_secret_access_key=SECRET_KEY
 )
 
-# # 4. GENERATE A FRESH TOKEN (Translating that PHP script into Python)
-# print("Generating fresh API access token...")
-# token_payload = {
-#     "grant_type": "client_credentials",
-#     "client_id": CLIENT_ID,
-#     "client_secret": CLIENT_SECRET
-# }
-# token_response = requests.post(TOKEN_URL, data=token_payload)
-# token_response.raise_for_status() # This will throw an error if the credentials fail
-# fresh_access_token = token_response.json().get("access_token")
-
-# # 5. Pull ACTUAL data from the API using the fresh token
-# print("Fetching live data from Trestle API...")
-# api_headers = {
-#     "Authorization": f"Bearer {fresh_access_token}",
-#     "Accept": "application/json"
-# }
-# response = requests.get(API_URL, headers=api_headers)
-# response.raise_for_status()
-# real_estate_data = response.json()
-
-# # Convert the Python dictionary into a formatted JSON string
-# json_string = json.dumps(real_estate_data, indent=2)
-
-# # 6. Upload directly to S3
-# print("Uploading JSON data to S3...")
-# s3.put_object(
-#     Bucket=BUCKET_NAME,
-#     Key='property_data_batch_2.json', # The name of the file in the cloud
-#     Body=json_string,                 # The actual JSON data
-#     ContentType='application/json'    # Tells S3 this is a JSON file
-# )
-# print("Upload successful! Your JSON data is now in the cloud.")
-
-
-
 def run_ingestion():
     try:
         # 4. GENERATE A FRESH TOKEN 
